19f790fdfafc49b58baad00bb967c63692e3c893
Jerry <Jerrytr@users.noreply.github.com>
2018-01-30 16:19:49 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,28 @@
 function myMap() {
-    var mapProp= {
-        center:new google.maps.LatLng(37.237162,-115.4840),
-        zoom:11,
+    let mapProp= {
+        center: new google.maps.LatLng(37.237162, -115.4840),
+        zoom: 11,
     };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    var geocoder = new google.maps.Geocoder();
+    let map = new google.maps.Map(document.getElementById('googleMap'),
+    mapProp);
+    let geocoder = new google.maps.Geocoder();
 
-    document.getElementById("submit").addEventListener("click", function() {
+    document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
     });
 }
 
 function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById("address").value;
-    geocoder.geocode({"address" : address}, function(results, status) {
-        if (status == "OK") {
+    let address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status == 'OK') {
             resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
+            let marker = new google.maps.Marker({
                 map: resultsMap,
-                position: results[0].geometry.location
+                position: results[0].geometry.location,
             });
         } else {
-            alert("There was an error: " + status);
+            alert('There was an error: ' + status);
         }
     });
 }
